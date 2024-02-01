c7109688e592fe077165fdc61e39694374b667e8
man-ishh <manishd.kumar@cogoport.com>
2024-02-01 12:35:31 +0530

    count fixes

--- src/Components/ListTask/Header.js
+++ src/Components/ListTask/Header.js
@@ -1,7 +1,7 @@
 const Header = ({text, bg, count})=>{
     return(
         <div className={`${bg} flex items-center h-12 pl-4 rounded-md uppercase text-sm text-white`}>
-           {text} <div className=' flex ml-2 text-black bg-white w-5 h-5 rounded-full items-center justify-center'> {count}</div>  
+           {text} { count >0 &&<div className=' flex ml-2 text-black bg-white w-5 h-5 rounded-full items-center justify-center'> {count}</div>}  
         </div>
     )
 }
